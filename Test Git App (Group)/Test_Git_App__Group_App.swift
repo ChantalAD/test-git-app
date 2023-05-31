92e62a00d72fb75a662526647bd81e3e4d636398
ChantalAD <chantal.alayna@gmail.com>
2023-05-31 13:50:23 +0800

    lol

---
+++ Test Git App (Group)/Test_Git_App__Group_App.swift
@@ -0,0 +1,17 @@
+//
+//  Test_Git_App__Group_App.swift
+//  Test Git App (Group)
+//
+//  Created by Chantal on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_Git_App__Group_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
